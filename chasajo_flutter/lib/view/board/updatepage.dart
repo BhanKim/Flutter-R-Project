b8af4eb94f9a66b5d9ae9bc25933b58e69cc1f48
javanyam <elyje5907@gmail.com>
2023-01-10 21:40:26 +0900

    fix board
    detail page

---
+++ chasajo_flutter/lib/view/board/updatepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UpdatePage extends StatelessWidget {
+  const UpdatePage({
+    super.key,
+    required this.id,
+  });
+  final String id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
